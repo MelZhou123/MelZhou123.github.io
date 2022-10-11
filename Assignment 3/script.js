09bbf38461bf897d7f96629a12e321e6c756007f
MelZhou123 <110432792+MelZhou123@users.noreply.github.com>
2022-10-11 14:40:30 +1100

    Add files via upload

---
+++ Assignment 3/script.js
@@ -0,0 +1,6 @@
+function fadeIn(){
+    document.getElementById("descriptions").style.opacity=1;
+    console.log ("test")
+}
+
+/* This function allows for the fade in function of text to operate on the webpage. */
