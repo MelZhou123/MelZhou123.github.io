1c0ef53d8ae2960507b9a58bc253d57bb33b2862
MelZhou123 <110432792+MelZhou123@users.noreply.github.com>
2022-10-13 22:33:25 +1100

    Add files via upload

---
+++ Assignment 3/script.js
@@ -0,0 +1,12 @@
+function fadeIn(){
+    document.getElementById("descriptions").style.opacity=1;
+    console.log ("test")
+}
+
+/* The fade in function permits the words to spray in and out which is a design decision I made to replicate the dissolving of particles into the air when a perfume bottle is sprayed. */
+
+
+
+
+
+
